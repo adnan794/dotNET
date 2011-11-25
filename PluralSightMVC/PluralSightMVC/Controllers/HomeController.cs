9b7cc3f4c6c5b906790639c6990166f5cef0b582
Adnan Babar <a.babar@amaze.com>
2011-11-25 17:28:34 +0000

    Added test project for .net mvc3

---
+++ PluralSightMVC/PluralSightMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PluralSightMVC.Models;
+
+namespace PluralSightMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = string.Format("{0}::{1}{2}",
+                                RouteData.Values["controller"],
+                                RouteData.Values["action"],
+                                RouteData.Values["id"]
+
+                                );
+                
+
+            var model = new ResturantReview()
+            {
+                Name = "Iqbal Tikka",
+                Rating = 8
+            };
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ PluralSightMVC/PluralSightMVC/Models/ResturantReview.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PluralSightMVC.Models
+{
+    public class ResturantReview
+    {
+        public string Name { get; set; }
+        public int Rating  {get; set;} 
+    }
+}
